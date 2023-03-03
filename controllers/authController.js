805692e71c32269ad865deaca7486ff1402e0832
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-04 00:38:49 +0100

    fix: refactor code

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,6 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
 import AppError from '../errors/appError.js';
 import sendEmail from '../utils/email.js';
-import ForbiddenError from '../errors/forbidden.js';
 import NotFoundError from '../errors/notFound.js';
 import asyncMiddleware from '../utils/asyncMiddleware.js';
 import UnauthenticatedError from '../errors/unauthenticated.js';
