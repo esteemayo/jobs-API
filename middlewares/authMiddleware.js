87f56be6b44ab13fc48951617cb330220ea89d03
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-04 00:24:15 +0100

    fix: format code

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -53,13 +53,14 @@ export const protect = asyncMiddleware(async (req, res, next) => {
   next();
 });
 
-export const restrictTo = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new ForbiddenError('You do not have permission to perform this action')
-      );
-    }
-    next();
-  };
-};
+export const restrictTo =
+  (...roles) =>
+    (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        return next(
+          new ForbiddenError('You do not have permission to perform this action')
+        );
+      }
+      next();
+    };
+
