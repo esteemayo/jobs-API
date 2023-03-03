a98230c1d50f5e04750b6cc578faaf1587f55105
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-04 00:16:41 +0100

    fix: modify

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,5 @@
 import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
-import { StatusCodes } from 'http-status-codes';
 
 import User from '../models/User.js';
 import asyncMiddleware from '../utils/asyncMiddleware.js';
