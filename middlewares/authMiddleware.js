e9875a82c5ae28fad147e3807c9a5af89cfee55e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-04 00:16:19 +0100

    feat: add protect auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,3 +5,50 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
 import asyncMiddleware from '../utils/asyncMiddleware.js';
 import UnauthenticatedError from '../errors/unauthenticated.js';
+
+export const protect = asyncMiddleware(async (req, res, next) => {
+  // getting token and check if it's there
+  let token;
+
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    token = authHeader.split(' ')[1];
+    // console.log(token);
+  }
+
+  if (!token) {
+    return next(
+      new UnauthenticatedError(
+        'You are not logged in! Please log in to get access'
+      )
+    );
+  }
+
+  // verify token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  // console.log(decoded);
+
+  // check if user still exists
+  const currentUser = await User.findById(decoded.id).select('-password');
+
+  if (!currentUser) {
+    return next(
+      new UnauthenticatedError(
+        'The user belonging to this token does no longer exist'
+      )
+    );
+  }
+
+  // check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new UnauthenticatedError(
+        'User recently changed password! Please log in again'
+      )
+    );
+  }
+
+  // grant access to protected routes
+  req.user = currentUser;
+  next();
+})
