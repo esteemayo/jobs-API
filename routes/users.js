4c12b6eb1ffefd55d8b9498c64e99f227da27111
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-04 00:27:38 +0100

    feat: add authMiddleware

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import * as authController from '../controllers/authController.js';
+import * as authMiddleware from '../middlewares/authMiddleware.js';
 import * as userController from '../controllers/userController.js';
 
 const router = express.Router();
